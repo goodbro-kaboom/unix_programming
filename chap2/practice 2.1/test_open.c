42000dd051971475b43da87674e94a71c8fcaddb
goodbro-kaboom <33711953+goodbro-kaboom@users.noreply.github.com>
2017-11-29 19:13:12 +0900

    연습문제 2.1
    test_open.c : source code
    test_open : gcc 로 컴파일한 실행파일
    result.txt  :  실행결과를 파이프를 통해 저장

--- chap2/practice 2.1/test_open.c
+++ chap2/practice 2.1/test_open.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
-#include <fcnt1.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 char *workfile = "junk";
 
 int main(){
   int filedes;
   
-  if(filedes = open(workfile, O_RDWR)) == -1){
+  if((filedes = open(workfile, O_RDWR)) == -1){
     printf("Couldn't open %s\n", workfile);
     exit(1);
   }
